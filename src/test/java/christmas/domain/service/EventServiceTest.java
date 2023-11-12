532a022a448989b4eba88771c6185d5591d5ede6
Jo TaeHyeong <wpg.jth@gmail.com>
2023-11-12 15:20:26 +0900

    test(EventServiceTest): 테스트 예상 검증값 로직 수정

--- src/test/java/christmas/domain/service/EventServiceTest.java
+++ src/test/java/christmas/domain/service/EventServiceTest.java
@@ -7,8 +7,11 @@ import christmas.domain.eventplanner.Event;
 import christmas.domain.eventplanner.EventBadge;
 import christmas.domain.restaurant.Menu;
 import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -36,18 +39,19 @@ class EventServiceTest {
     }
 
     @Test
-    void 증정_이벤트_적용시_샴페인_1개를_증정한다() {
+    void 증정_이벤트_적용시_증정품을_제공한다() {
         //given
         Customer customer = Customer.reserveVisit(3, defaultMenus);
-        Map<Menu, Integer> expectedGift = Map.of(Menu.CHAMPAGNE, 1);
+        Set<Menu> allMenus = EnumSet.allOf(Menu.class);
 
         //when
-        List<Event> events = eventService.findEventByCustomer(customer);
+        List<Event> events = eventService.findEventsByCustomer(customer);
         Map<Menu, Integer> gift = eventService.getGiftMenu(customer);
 
         //then
         assertThat(events).contains(Event.GIFT_EVENT);
-        assertThat(gift).isEqualTo(expectedGift);
+        assertThat(gift.keySet()).allMatch(allMenus::contains);
+        assertThat(gift.values()).allMatch(count -> count > 0);
     }
 
     @Test
@@ -57,7 +61,7 @@ class EventServiceTest {
         Customer customer = Customer.reserveVisit(3, menus);
 
         //when
-        List<Event> events = eventService.findEventByCustomer(customer);
+        List<Event> events = eventService.findEventsByCustomer(customer);
         Map<Menu, Integer> gift = eventService.getGiftMenu(customer);
 
         //then
@@ -69,13 +73,9 @@ class EventServiceTest {
     void 고객정보를_받으면_혜택_내역을_반환한다() {
         //given
         Customer customer = Customer.reserveVisit(24, defaultMenus);
+        List<Event> events = eventService.findEventsByCustomer(customer);
         Map<Menu, Integer> giftMenu = eventService.getGiftMenu(customer);
-        Map<Event, Integer> expectedBenefitDetails = Map.of(
-                Event.CHRISTMAS_D_DAY_DISCOUNT, Event.CHRISTMAS_D_DAY_DISCOUNT.calculate(customer),
-                Event.WEEKDAY_DISCOUNT, Event.WEEKDAY_DISCOUNT.calculate(customer),
-                Event.SPECIAL_DISCOUNT, Event.SPECIAL_DISCOUNT.calculate(customer),
-                Event.GIFT_EVENT, getGiftAmount(giftMenu)
-        );
+        Map<Event, Integer> expectedBenefitDetails = getExpectedBenefitDetails(events, customer, giftMenu);
 
         //when
         Map<Event, Integer> benefitDetails = eventService.getBenefitDetails(customer);
@@ -84,11 +84,26 @@ class EventServiceTest {
         assertThat(benefitDetails).isEqualTo(expectedBenefitDetails);
     }
 
+    private Map<Event, Integer> getExpectedBenefitDetails(
+            List<Event> events,
+            Customer customer,
+            Map<Menu, Integer> giftMenu
+    ) {
+        Map<Event, Integer> expectedBenefitDetails = new HashMap<>();
+        for (Event event : events) {
+            expectedBenefitDetails.put(event, event.calculate(customer));
+        }
+        if (events.contains(Event.GIFT_EVENT)) {
+            expectedBenefitDetails.put(Event.GIFT_EVENT, getGiftAmount(giftMenu));
+        }
+        return expectedBenefitDetails;
+    }
+
     @Test
     void 고객정보를_받으면_총혜택_금액을_반환한다() {
         //given
         Customer customer = Customer.reserveVisit(24, defaultMenus);
-        List<Event> events = eventService.findEventByCustomer(customer);
+        List<Event> events = eventService.findEventsByCustomer(customer);
         int expectedDiscountAmount = discountAmount(events, customer);
         int expectedGiftAmount = getGiftAmount(eventService.getGiftMenu(customer));
 
@@ -115,7 +130,7 @@ class EventServiceTest {
     void 고객정보를_받으면_할인_후_예상_결제_금액을_계산한다() {
         //given
         Customer customer = Customer.reserveVisit(24, defaultMenus);
-        List<Event> events = eventService.findEventByCustomer(customer);
+        List<Event> events = eventService.findEventsByCustomer(customer);
         int orderAmount = customer.getOrderAmount();
         int discountAmount = discountAmount(events, customer);
 
